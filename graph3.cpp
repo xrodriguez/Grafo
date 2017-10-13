9859c1300a26476fdc7ecd3e0c3ee5d69fc156c2
Xavier Rodriguez <exrodriguezc@gmail.com>
2017-10-13 15:45:29 -0500

    cambios

--- graph3.cpp
+++ graph3.cpp
@@ -176,17 +176,7 @@ int main()
 {
     // create the graph given in above fugure
     int numberNodes = 10;
-    // vector< vector <double> > data = {    {8825224.158,276682.7726},
-    //                                         {8731925.253,323651.0204},
-    //                                         {8714685.216,317992.8898},
-    //                                         {8674105.475,299797.1793},
-    //                                         {8677175.367,307975.169},
-    //                                         {8696761.014,271186.3761},
-    //                                         {8806301.525,291282.6593},
-    //                                         {8820167.84,306122.8728},
-    //                                         {8758790.967,239770.5303},
-    //                                         {8787311.209,312266.0346}
-    //                                     };
+
     vector< vector <double> > dataEdges {
         {1,2},
         {2,3},
@@ -215,11 +205,10 @@ int main()
 		std::cout<<"Not FILE"<<std::endl;//file doesn't exist
 	}
 	char tmp;
-	float x2=0, y2=0, t;
+	float x,y,t;
 	int i=0;
-	while(read >> t>>tmp>>x2>>tmp>>y2){
-		Node node(x2,y2,i);
-        nodes.push_back(node);
+	while(read >> t>>tmp>>x>>tmp>>y){
+        nodes.push_back(Node(x,y,i));
 		i++;
 	}
 
