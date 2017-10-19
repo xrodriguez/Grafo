989e8fe4535c357f57ddef2faeb5e06473fa22c5
johan2008 <josecastroochante@gmail.com>
2017-10-19 16:33:34 -0500

    change12

--- Heuristics.cpp
+++ Heuristics.cpp
@@ -179,6 +179,103 @@ vector<Node>  Heuristics::Astar(Graph graph , Node start,Node end){
 }
 
 
+vector<Node>  Heuristics::BidirectionalAstar(Graph graph , Node start,Node end){
+	vector<Node> failure ;
+	cout<<  "NODOS: " <<start.tag<<" - "<<end.tag<<endl;
+	double tentative_g;
+   	map<int,Node*> openMap;
+   	map<int,Node*> closedMap;
+   	map<int,Node*> openMap2;
+   	map<int,Node*> closedMap2;
+
+   	map<int, Node> map_node;
+
+	start.g = 0 ;
+	openMap[start.tag] = &start;
+	start.f = calculateDistance(start,end);
+	
+	while(!openMap.empty() && !openMap2.empty()  ){
+		
+		//to first process
+		Node* bestNode = NULL;		
+		map<int,Node* >::iterator _it;
+		for ( _it = openMap.begin(); _it != openMap.end(); ++_it ){
+            Node* candidate = _it->second;
+            if ( bestNode == NULL ){
+            	bestNode = candidate;
+            }
+            else if ( candidate->f < bestNode->f ){
+                	bestNode = candidate;
+            }
+        }
+
+        Node current = *(bestNode);
+        openMap.erase( current.tag );
+        closedMap[current.tag] = &current;
+
+        map<int, Node>::iterator iter ;
+
+        if(current.tag == end.tag) {
+        	cout <<"Ruta:::::::::::::::::::::::::::::::::::::::::::::::::::::: "<<"FIN"<<endl; 
+        	return reconstruct_path(map_node,current);
+        }
+
+
+		list<Node> neighbors = graph.getNeighbors(current);
+		for (auto neighbor = neighbors.begin(); neighbor !=neighbors.end(); neighbor++){
+			if (    !(closedMap.find( (*neighbor).tag ) == closedMap.end() ) ){
+                continue;
+            }
+
+			if(  (openMap.find( (*neighbor).tag ) == openMap.end())   ){
+				Node* neighborAux = new  Node((*neighbor).x , (*neighbor).y, (*neighbor).tag)  ;
+				openMap[(*neighbor).tag ] = neighborAux;
+			}
+
+			tentative_g = current.g + calculateDistance(*neighbor,current);
+			if (tentative_g >= (*neighbor).g){
+                continue;
+			}
+			map_node[(*neighbor).tag] = current;
+			(*neighbor).g = tentative_g;
+			(*neighbor).f = (*neighbor).g + calculateDistance(*neighbor,end);
+			Node neighborAux = Node((*neighbor).x , (*neighbor).y, (*neighbor).tag);
+			neighborAux.f = (*neighbor).f;
+			neighborAux.g = (*neighbor).g;
+			openMap[ (*neighbor).tag]->g = neighborAux.g;// = new Node((*neighbor).x , (*neighbor).y, (*neighbor).tag);// &neighborAux;
+			openMap[ (*neighbor).tag]->f = neighborAux.f; 
+        }
+
+
+
+        //to second process.
+
+
+		Node* bestNode = NULL;		
+		map<int,Node* >::iterator _it;
+		for ( _it = openMap2.begin(); _it != openMap2.end(); ++_it ){
+            Node* candidate = _it->second;
+            if ( bestNode == NULL ){
+            	bestNode = candidate;
+            }
+            else if ( candidate->f < bestNode->f ){
+                	bestNode = candidate;
+            }
+        }
+        Node current = *(bestNode);
+        openMap.erase( current.tag );
+        closedMap[current.tag] = &current;
+
+
+
+
+
+
+	}
+	return failure; 	
+}
+
+
 
 
 Heuristics::~Heuristics(){};
--- Heuristics.h
+++ Heuristics.h
@@ -6,6 +6,7 @@ class Heuristics
 	public:
 		Heuristics();
 		vector<Node> Astar(Graph graph , Node start,Node end);
+		vector<Node> BidirectionalAstar(Graph graph , Node start,Node end);
 		//vector<Node> reconstruct_path(map<int, Node> cameFrom, Node current);
 		bool haveEdge(int a, int b);
 		//void Dijkstra(Graph graph , Node start );
